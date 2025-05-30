fbe73981d8ef8cbf70920acddfa7207ac1cca798
Harald Gutmann <harald@gutmann.one>
2025-05-30 12:22:45 +0200

    fix visibility for rustls TlsAcceptor set_alpn()
    rustls TlsAcceptor update with_callbacks Todo/Error

--- pingora-core/src/listeners/tls/rustls/mod.rs
+++ pingora-core/src/listeners/tls/rustls/mod.rs
@@ -106,10 +106,9 @@ impl TlsSettings {
     where
         Self: Sized,
     {
-        // TODO: verify if/how callback in handshake can be done using Rustls
         Error::e_explain(
             InternalError,
-            "Certificate callbacks are not supported with feature \"rustls\".",
+            "Certificate callbacks are not supported when using rustls.",
         )
     }
 }
